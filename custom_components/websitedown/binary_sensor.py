d6c8ac9b264a66b5a2b1cf444f5f912921debbf8
Michel van de Wetering <michel.van.de.wetering@gmail.com>
2020-07-24 19:05:14 +0200

    Switch to aiohttp

--- custom_components/websitedown/binary_sensor.py
+++ custom_components/websitedown/binary_sensor.py
@@ -1,10 +1,11 @@
 """Platform for sensor integration."""
 
 import logging
-import requests
 import voluptuous as vol
 from datetime import timedelta
 from urllib.parse import urlparse
+
+import aiohttp
 
 from homeassistant.components.binary_sensor import (
     DEVICE_CLASS_PROBLEM,
@@ -16,6 +17,7 @@ from homeassistant.const import (
     CONF_NAME
 )
 from homeassistant.helpers import (config_validation as cv)
+from homeassistant.helpers.aiohttp_client import async_get_clientsession
 
 CONF_WEBSITES = "websites"
 
@@ -37,29 +39,32 @@ PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
 
 SCAN_INTERVAL = timedelta(minutes=10)
 
-def setup(hass, config):
+async def async_setup(hass, config):
     """Set up the sensor platform."""
     return True
 
-def setup_platform(hass, config, add_entities, discovery_info=None):
+async def async_setup_platform(hass, config, add_entities, discovery_info=None):
     """Set up the sensor platform."""
     entities = []
     websites = config.get(CONF_WEBSITES)
+    websession = async_get_clientsession(hass)
+
     for website in websites:
         url = website.get(CONF_URL)
         name = website.get(CONF_NAME, urlparse(url).netloc)
-        entities.append(WebsiteDownSensor(url, name))
+        entities.append(WebsiteDownSensor(websession, url, name))
     add_entities(entities, True)
 
 
 class WebsiteDownSensor(BinarySensorEntity):
     """Representation of a Sensor."""
 
-    def __init__(self, url, name):
+    def __init__(self, websession, url, name):
         """Initialize the sensor."""
         self._is_down = None
         self._url = url
         self._name = name
+        self._websession = websession
 
     @property
     def name(self):
@@ -81,11 +86,11 @@ class WebsiteDownSensor(BinarySensorEntity):
         """Return the class of this device, from component DEVICE_CLASSES."""
         return DEVICE_CLASS_PROBLEM
 
-    def update(self):
+    async def async_update(self):
         """Do a request to the website """
         try:
-            resp = requests.get(self._url)
-            _LOGGER.debug(resp)
-            self._is_down = resp.status_code >= 500
-        except TimeoutError:
+            async with self._websession.get(self._url) as resp:
+                _LOGGER.debug(resp)
+                self._is_down = resp.status >= 500
+        except aiohttp.ClientConnectionError:
             self._is_down = True
